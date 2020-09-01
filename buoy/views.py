166d90c23cfe775b331e2e585922a63b761a9832
Brennan <bmtymrak@gmail.com>
2020-09-01 16:10:36 -0400

    First commit of fully funcational app

---
+++ buoy/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+import requests
+import datetime
+from dateutil import tz
+
+
+def get_buoy_data(url, name):
+    r = requests.get(url)
+
+    x = 0
+    data = []
+    for line in r.iter_lines(decode_unicode=True):
+        data.append(line.split())
+        x += 1
+        if x == 4:
+            break
+
+    utc_tz = tz.gettz('UTC')
+    et_tz = tz.gettz('America/Detroit')
+    time_utc = datetime.datetime.strptime(f'{data[2][1]}/{data[2][2]}/{data[2][0]} {data[2][3]}:{data[2][4]}', '%m/%d/%Y %H:%M')
+    time_utc.replace(tzinfo=utc_tz)
+    time_eastern = time_utc.astimezone(et_tz)
+
+    info = {
+            f'{name}_time': f'{time_eastern:%m/%d/%Y %H:%M} ET',
+            f'{name}_wind_direction': data[2][5],
+            f'{name}_wind_speed': round(float(data[2][6])/.44704, 2),
+            f'{name}_wind_gusts': round(float(data[2][7])/.44704, 2),
+            f'{name}_wave_height': round(float(data[2][8])*3.2808, 2),
+            f'{name}_air_temp': round(float(data[2][13])*1.8 + 32, 2),
+        }
+
+    return info
+
+
+def home_page_view(request):
+    info = {}
+
+    north_data = get_buoy_data('https://www.ndbc.noaa.gov/data/realtime2/45023.txt', 'north')
+    south_data = get_buoy_data('https://www.ndbc.noaa.gov/data/realtime2/45025.txt', 'south')
+    info.update(north_data)
+    info.update(south_data)
+
+    return render(request, 'main.html', context=info)
