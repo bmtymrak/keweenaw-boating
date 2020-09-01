141c16764908c0ef0505c914ae98a08efee89007
Brennan <bmtymrak@gmail.com>
2020-09-01 17:17:45 -0400

    Updated Allowed Hosts

--- keweenaw_boating/settings.py
+++ keweenaw_boating/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['keweenaw-boating.herokuapp.com']
+ALLOWED_HOSTS = ['keweenaw-boating.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
